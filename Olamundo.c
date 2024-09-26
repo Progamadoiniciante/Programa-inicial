3e649081c434cf716e68afcc3256c5712aa607b9
Progamadoiniciante <felipesantana10072005@gmail.com>
2024-09-26 18:46:47 +0000

    o que eu consegue fazer com o printf e scanf no tablet

--- Olamundo.c
+++ Olamundo.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+int main() {
+
+int idade;
+int matricula;
+float altura;
+
+printf("ola aque é o mundo eu queria saber sua idade?\n");
+scanf("%d", &idade);
+
+printf("agora sua matricula:\n");
+scanf("%d", &matricula);
+
+printf("agora sua altura:\n");
+scanf("%f", &altura);
+
+printf("sua idade è:%d anos\nsua matricula é:%d\nsua altura é:%.2f\n", idade, matricula, altura);
+
+    return 0;
+}
 
